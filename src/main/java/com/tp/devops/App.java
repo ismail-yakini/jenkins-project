df8933173c9562de63de065fa4390fa00d1aae85
Ismail Yakini <ismailyakini5@gmail.com>
2026-04-10 12:02:06 +0100

    make another modification to App.java

--- src/main/java/com/tp/devops/App.java
+++ src/main/java/com/tp/devops/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
 	System.out.println("this is a modification !");
-    }
+	System.out.println("Another modification !");
+  }
 }
